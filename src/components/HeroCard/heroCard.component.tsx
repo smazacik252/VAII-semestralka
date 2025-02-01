7e8608e2cfb5cb7ff64739962c532316b9caabda
Smazacik <j.vavrik252@gmail.com>
2025-02-01 23:41:34 +0100

    added routing

--- src/components/HeroCard/heroCard.component.tsx
+++ src/components/HeroCard/heroCard.component.tsx
@@ -1,4 +1,5 @@
 import {HeroCardStyled} from "./heroCard.styled.tsx";
+import {Link} from "react-router-dom";
 
 type HeroCardProps = {
     id: string;
@@ -8,12 +9,15 @@ type HeroCardProps = {
 };
 
 export const HeroCard:  React.FC<HeroCardProps> = ({name, image}) => {
+    const formattedName = name.toLowerCase().replace(/\s+/g, "-");
     return (
-        <HeroCardStyled>
-            <div className="hero-portrait">
-                <img src={image} alt={name}/>
-            </div>
-            <h3>{name}</h3>
-        </HeroCardStyled>
+        <Link to={`/hrdinovia/${formattedName}`} style={{ textDecoration: "none" }}>
+            <HeroCardStyled>
+                <div className="hero-portrait">
+                    <img src={image} alt={name} />
+                </div>
+                <h3>{name}</h3>
+            </HeroCardStyled>
+        </Link>
     );
 };
