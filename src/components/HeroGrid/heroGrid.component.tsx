d5dcf7d893cd7e410374a0c33a56d27319956527
Smazacik <j.vavrik252@gmail.com>
2025-02-05 16:56:49 +0100

    added fetching heroes from backend

--- src/components/HeroGrid/heroGrid.component.tsx
+++ src/components/HeroGrid/heroGrid.component.tsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {HeroGridStyled} from "./heroGrid.styled.tsx";
 import {HeroCard} from "../HeroCard/heroCard.component.tsx";
+import {Alert, CircularProgress} from "@mui/material";
 
 type Hero = {
     id: string;
     name: string;
-    image: string;
+    portrait: string;
 };
 
-type HeroGridProps = {
-    heroes: Hero[];
-};
 
-export const HeroGrid: React.FC<HeroGridProps> = ({heroes}) => {
+export const HeroGrid: React.FC = () => {
+
+    const [heroes, setHeroes] = useState<Hero[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchHeroes = async () => {
+            try {
+                const response = await fetch('http://localhost:8000/heroes');
+                if (!response.ok) throw new Error("Nepodarilo sa nacitat hrdinov");
+                const data = await response.json();
+                setHeroes(data);
+            } catch (err: any) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchHeroes();
+    }, []);
+
+    if (loading) return <CircularProgress/>;
+    if (error) return <Alert severity="error">Chyba:{error}</Alert>
+
     return(
         <HeroGridStyled>
             <h1>Hrdinovia</h1>
@@ -21,7 +43,7 @@ export const HeroGrid: React.FC<HeroGridProps> = ({heroes}) => {
                     key={hero.id}
                     id={hero.id}
                     name={hero.name}
-                    image={hero.image}
+                    image={hero.portrait}
                 />
             ))}
         </HeroGridStyled>
