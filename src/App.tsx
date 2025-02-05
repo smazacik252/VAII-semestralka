ef7d6302b191cd9852961687f17764716a4d02e0
Smazacik <j.vavrik252@gmail.com>
2025-02-05 16:57:50 +0100

    changed routing to match new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
 import './App.css'
-import {RegisterForm} from "./components/RegisterForm/registerForm.components.tsx";
-import {Header} from "./components/Header/header.compontent.tsx";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {RegisterForm} from "./components/RegisterForm/registerForm.components.tsx";
 import {UserList} from "./components/UserList/userList.component.tsx";
 import {LoginForm} from "./components/LoginForm/loginForm.component.tsx";
 import {useState} from "react";
-import {MainComponent} from "./components/MainContent/mainContent.component.tsx";
-import {HeroCard} from "./components/HeroCard/heroCard.component.tsx";
+import {HeroGrid} from "./components/HeroGrid/heroGrid.component.tsx";
+import {HeroView} from "./components/HeroView/heroView.component.tsx";
+import {ItemTab} from "./components/ItemTab/itemTab.component.tsx";
+import {Layout} from "./components/Layout/laoyout.component.tsx";
+import {LatestArticles} from "./components/LatestArticles/latestArticles.component.tsx";
 
 function App() {
     const [user, setUser] = useState<any>(JSON.parse(localStorage.getItem("user") || "null"));
-  return (
-    <>
+    return (
         <Router>
-            <Header user={user} setUser={setUser}/>
-                <Routes>
-                    <Route path = "/"/>
-                    <Route path = "/prihlasenie" element ={<LoginForm/>}/>
-                    <Route path = "/registracia" element ={<RegisterForm/>}/>
+            <Routes>
+                <Route element={<Layout user={user} setUser={setUser} />}>
+                    <Route path="/" element={<LatestArticles />} />
+                    <Route path="/prihlasenie" element={<LoginForm />} />
+                    <Route path="/registracia" element={<RegisterForm />} />
                     <Route path="/pouzivatelia" element={<UserList user={user} setUser={setUser} />} />
-                 </Routes>
-            <MainComponent></MainComponent>
-            <HeroCard
-                id="1"
-                name="Abrams"
-                image="../img/heroes/Abrams_card.png"
-            ></HeroCard>
-            <footer>All images, characters, and assets used on this website belong to Valve Corporation. They are used here solely for educational purposes and non-commercial use. </footer>
+                    <Route path="/predmety" element={<ItemTab />} />
+                    <Route path="/hrdinovia" element={<HeroGrid />} />
+                    <Route path="/hrdinovia/:name" element={<HeroView />} />
+                </Route>
+            </Routes>
         </Router>
-    </>
-  );
+    );
 }
 
 export default App
