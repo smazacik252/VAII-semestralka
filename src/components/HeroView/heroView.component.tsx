fb7bf4bcefda90ec65063a072069074fd6ebd319
Smazacik <j.vavrik252@gmail.com>
2025-02-05 19:49:20 +0100

    added fetching hero from backend

--- src/components/HeroView/heroView.component.tsx
+++ src/components/HeroView/heroView.component.tsx
@@ -1,30 +1,67 @@
 import {HeroViewStyled} from "./heroView.styled.tsx";
 import {StatCard} from "../StatCard/statCard.component.tsx";
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {Alert, CircularProgress} from "@mui/material";
+
+type Hero = {
+    id: string;
+    name: string;
+    render: string;
+    info: string;
+    lore: string;
+    statsWeapon: {
+        [key: string]: number;
+    };
+
+    statsVitality: {
+        [key: string]: number;
+    };
+}
 
 export const HeroView = () => {
+
+    const {name} = useParams<{ name : string }>();
+    const [hero, setHero] = useState<Hero | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchHero = async () => {
+            try {
+                const response = await fetch(`http://localhost:8000/heroes/${name}`);
+                if (!response.ok) throw new Error("Nepodarilo sa nacitat hrdinu");
+                const data = await response.json();
+                setHero(data);
+            } catch (err: any) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchHero();
+    }, [name])
+
+    if (loading) return <CircularProgress/>;
+    if (error) return <Alert severity="error">Chyba:{error}</Alert>
+    if (!hero) return <Alert severity="error">Hrdina nenajdeny</Alert>
+
     return (
         <HeroViewStyled>
             <div className = "container">
                 <div className="up-container">
                     <div className="left-container">
-                        <h1>Abrams</h1>
-                        <p> Abrams has the bulk and sustain to lead from the front, often running into the middle of
-                            his
-                            foes and watching them scatter. If his enemies waste their fire on him, his backline
-                            teammates can lay out damage with impunity.</p>
+                        <h1>{hero.name}</h1>
+                        <p> {hero.info}</p>
                         <div className="stats-container">
                             <div className="stat-container">
                                 <div className="stat-name">
                                     <h2>Weapon</h2>
                                 </div>
                                 <div className="cards-container">
-                                    <StatCard></StatCard>
-                                    <StatCard></StatCard>
-                                    <StatCard></StatCard>
-                                    <StatCard></StatCard>
-                                    <StatCard></StatCard>
-                                    <StatCard></StatCard>
-
+                                    {Object.entries(hero.statsWeapon).map(([statName, value]) => (
+                                        <StatCard key={statName} statName={statName} statValue={value} />
+                                    ))}
                                 </div>
                             </div>
                             <div className="stat-container">
@@ -32,26 +69,20 @@ export const HeroView = () => {
                                     <h2>Vitality</h2>
                                 </div>
                                 <div className="cards-container">
-                                    <StatCard></StatCard>
-                                    <StatCard></StatCard>
-                                    <StatCard></StatCard>
-                                    <StatCard></StatCard>
-                                    <StatCard></StatCard>
-                                    <StatCard></StatCard>
+                                    {Object.entries(hero.statsVitality).map(([statName, value]) => (
+                                        <StatCard key={statName} statName={statName} statValue={value} />
+                                    ))}
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="right-container">
-                        <img src="../../../img/Abrams_Render.png"/>
+                        <img src={hero.render}/>
                     </div>
                 </div>
                 <div className="lower-container">
                     <h1>Lore</h1>
-                    <p>Hard hitting, hard headed, and hard drinking; Detective Abrams has been a fixture on the New York investigating scene for years. From stolen art, to missing persons, to ritual murders; Abrams didn't just take any case that came across his desk... he solved them.
-                        But his days of following cheating spouses came to an end the day he opened his office door and found The Tome sitting on his desk.
-                        No instruction was left for him save for a brief note scrawled in onyx blood that read "Don't let them have it". Abrams hasn't figured out where the thing came from; but seeing as his home has been broken into, his office tossed on 3 different occasions, and his car firebombed he has a vested interest in figuring out what the hell is going on.
-                    </p>
+                    <p>{hero.lore}</p>
                 </div>
             </div>
 
