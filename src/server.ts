046e7b3c41f5b788c3da6a8abc588f2edb09bab2
Smazacik <j.vavrik252@gmail.com>
2025-02-06 19:00:39 +0100

    added item and article routes

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import connection from './db';
 import userRoutes from "./routes/user.routes";
 import cors from 'cors';
 import heroRoutes from "./routes/hero.routes";
+import itemRoutes from "./routes/item.routes";
+import articleRoutes from "./routes/article.routes";
 
 const port = 8000;
 const app = express();
@@ -15,7 +17,9 @@ app.get('/', (req: Request, res: Response) => {
 });
 app.use(cors());
 app.use('/users', userRoutes);
-app.use('/heroes', heroRoutes)
+app.use('/heroes', heroRoutes);
+app.use('/items', itemRoutes);
+app.use('/articles', articleRoutes);
 
 
 const startServer = async () => {
