a6b04fcf466533dac5106dfd14c257ff09c54006
Smazacik <j.vavrik252@gmail.com>
2025-02-06 19:02:06 +0100

    simplified relationship with article

--- src/entities/Comment.ts
+++ src/entities/Comment.ts
@@ -1,34 +1,25 @@
-import {Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, OneToMany} from "typeorm";
-import {Article} from "./Article";
+import {Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, BaseEntity
+} from "typeorm";
+import { Article } from "./Article";
 
 @Entity()
-export class Comment {
+export class Comment extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number = 0;
 
     @Column({ type: "text" })
     content: string = "";
 
-    @ManyToOne(() => Article, (article) => article.comments, { onDelete: "CASCADE" })
+    @ManyToOne(() => Article, (article) => article.comments, {
+        onDelete: "CASCADE",
+    })
+
     @JoinColumn({ name: "articleId" })
-    article!: Article;
-
-    @Column()
-    articleId: number = 0;
-
-    @ManyToOne(() => Comment, (comment) => comment.children, { nullable: true })
-    @JoinColumn({ name: "parentId" })
-    parent: Comment | null = null;
-
-    @Column({ nullable: true })
-    parentId: number | null = null;
+    article: Article | null = null;
 
     @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
     createdAt: Date = new Date();
 
     @Column({ type: "timestamp", nullable: true })
     updatedAt: Date | null = null;
-
-    @OneToMany(() => Comment, (comment) => comment.parent)
-    children: Comment[] = [];
 }
